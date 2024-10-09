215ca2d7476dc2bb8aee15c8403d5cf27ac3f376
Gulzarokz <gokz22301@gamil.com>
2024-10-09 22:39:13 +0500

    done Maths expersion & string cancatenation

---
+++ index.js
@@ -0,0 +1,74 @@
+// Chapter No 7
+
+// console.log(2 - 1 * 8); 
+// 1*8
+// 2-8
+//  -6
+// -6  * precendent is higher
+
+
+// console.log(1 + 2 * 3 + 7);
+// 1+6+7
+// 14
+
+
+
+// console.log(1 + 2 * (3 + 7));
+// 1 + 2 * 10
+// 1 + 20 
+// 21
+
+// console.log(5 * 5 -30/(3 + 7));
+// 5 * 5 - 30 / 10
+// 25 - 3
+// 3
+
+// console.log(5 * 5 -3 / 3 + 7);
+// 25 - 3 / 3 + 7
+// 25 - 1 + 7
+// 24 + 7
+// 31
+
+// console.log(2 * 4 *  4 + 2 );
+// 8 * 4 + 2
+// 32 + 2
+// // 34
+// var pressure = 4 / (2 * 4);
+// console.log(pressure);
+
+// chapter NO: 8 Concatenating text strings
+
+// console.log("Thanks for your inputs");
+// alert("Thanks for your inputs");
+
+// const messages = "Thank you for messages"
+// alert(messages);
+
+// const userName = "Gulzarkhan";
+// alert("Thank you " + userName + "!");
+
+// const firstName = "Gulzar";
+// const lastName = "khan";
+// const fullName = firstName + " " + lastName;
+// alert(fullName);
+
+// const messages = "Thanks ";
+// const userName = "Gulzar";
+// const punctuation = "!";
+// alert(messages  + userName + punctuation);
+
+// alert("Thanks "+ "ali"+ "!");
+
+// const messages = "Thanks ";
+// const userName = "Gulzar";
+// const punctuation = "!";
+// const customMessage = (messages  + userName + punctuation);
+// alert(customMessage);
+
+
+// if you number put in string it consider as string
+
+// alert("4" + "4");
+
+// var userName = ("3" + "userName");
+// alert(userName);
